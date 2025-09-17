b3e4e0dbc0586adb952fde3fe7e6ee19cb38a5f9
ALIJAYA JURAGAN PULSA <37974674+alijayanet@users.noreply.github.com>
2025-09-17 22:12:11 +0700

    Delete migrations directory

--- migrations/add_description_to_invoices.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add description column to invoices table
--- Date: 2025-01-27
--- Description: Add description column for storing invoice descriptions
-
-ALTER TABLE invoices ADD COLUMN description TEXT NULL;
